cd26407ae51ac40bfb58061c5dd9c352c5342e95
Henrikas Matusevicius <henrikass@embeduk.com>
2022-10-05 15:51:58 +0100

    Added SPI #defines

--- target/stm32f4xx_drivers/drivers/Src/stm32f407xx_spi_driver.c
+++ target/stm32f4xx_drivers/drivers/Src/stm32f407xx_spi_driver.c
@@ -7,6 +7,12 @@
 
 #include "stm32f407xx_spi_driver.h"
 
+/**
+ * @brief SPI Clock init
+ * 
+ * @param pSPIx 
+ * @param EnorDi 
+ */
 void SPI_PeriClockControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi)
 {
     if(EnorDi == ENABLE)
@@ -41,4 +47,37 @@ void SPI_PeriClockControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi)
     }
 }
 
+/**
+ * @brief SPI Init 
+ * 
+ * @param pSPIHandle 
+ */
+void SPI_Init(SPI_Handle_t *pSPIHandle)
+{
+    uint32_t temp = 0;
 
+    
+
+}
+
+/**
+ * @brief SPI De-Init
+ * 
+ * @param pSPIx 
+ */
+void SPI_DeInit(SPI_RegDef_t *pSPIx)
+{
+    /* Resetting all registers in the peripheral */
+    if(pSPIx == SPI1)
+    {
+        SPI1_REG_RESET();
+    }
+    else if(pSPIx == SPI2)
+    {
+        SPI2_REG_RESET();
+    }
+    else if(pSPIx == SPI3)
+    {
+        SPI3_REG_RESET();
+    }
+}
