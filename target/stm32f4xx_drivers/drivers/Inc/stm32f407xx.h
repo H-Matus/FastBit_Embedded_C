faf00e0750dcc4449db521c2b19d0549029981e0
Henrikas Matusevičius <matusevicius.henrikas@gmail.com>
2022-10-17 11:43:50 +0100

    Initialised CR1, CR2 and OAR1 registers of I2C Init

--- target/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h
+++ target/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h
@@ -322,6 +322,10 @@ typedef struct
 #define SPI2_REG_RESET()    do{ (RCC->APB1RSTR |= (1 << 14)); (RCC->APB1RSTR &= ~(1 << 14)); }while(0)
 #define SPI3_REG_RESET()    do{ (RCC->APB1RSTR |= (1 << 15)); (RCC->APB1RSTR &= ~(1 << 15)); }while(0)
 
+#define I2C1_REG_RESET()    do{ (RCC->APB1RSTR |= (1 << 21)); (RCC->APB1RSTR &= ~(1 << 21)); }while(0)
+#define I2C2_REG_RESET()    do{ (RCC->APB1RSTR |= (1 << 22)); (RCC->APB1RSTR &= ~(1 << 22)); }while(0)
+#define I2C3_REG_RESET()    do{ (RCC->APB1RSTR |= (1 << 23)); (RCC->APB1RSTR &= ~(1 << 23)); }while(0)
+
 /***
  * returns port code for given GPIOx base address
  */
--- target/stm32f4xx_drivers/drivers/Src/stm32f407xx_i2c_driver.c
+++ target/stm32f4xx_drivers/drivers/Src/stm32f407xx_i2c_driver.c
@@ -7,6 +7,9 @@
 
 #include "stm32f407xx_i2c_driver.h"
 
+uint16_t AHB_PreScaler[8] = {2, 4, 8, 16, 64, 128, 256, 512};
+uint16_t APB1_PreScaler[4] = {2, 4, 8, 16};
+
 void I2C_PeriClockControl(I2C_RegDef_t *pI2Cx, uint8_t EnorDi)
 {
     if(EnorDi == ENABLE)
@@ -39,6 +42,78 @@ void I2C_PeriClockControl(I2C_RegDef_t *pI2Cx, uint8_t EnorDi)
             I2C3_PCLK_DI();
         }
     }
+}
+
+uint32_t RCC_GetPLLOutput()
+{
+    return;
+}
+
+uint32_t RCC_GetPCLK1Value(void)
+{
+    uint32_t pclk1, SystemClk;
+    uint8_t clksrc, temp, ahbp, apb1p;
+    clksrc = (( RCC->CFGR >> 2 ) & 0x3 );
+
+    if( clksrc == 0 )
+    {
+        SystemClk = 16000000;
+    }
+    else if( clksrc == 1 )
+    {
+        SystemClk = 8000000;
+    }
+    else if( clksrc == 2 )
+    {
+        SystemClk = RCC_GetPLLOutput();
+    }
+
+    temp = ( ( RCC->CFGR >> 4 ) & 0xF );
+
+    // AHB Prescaler calculations
+    if(temp < 8)
+    {
+        ahbp = 1;
+    }
+    else
+    {
+        ahbp = AHB_PreScaler[temp-8];
+    }
+
+    temp = ( ( RCC->CFGR >> 10 ) & 0x7 );
+
+    // APB Prescaler calculations
+    if(temp < 4)
+    {
+        apb1p = 1;
+    }
+    else
+    {
+        apb1p = APB1_PreScaler[temp-4];
+    }
+
+    pclk1 = ( SystemClk / ahbp ) / apb1p;
+
+    return pclk1;
+}
+
+void I2C_Init(I2C_Handle_t *pI2CHandle)
+{
+    uint32_t tempreg = 0;
+
+    // ACK control bit (CR1)
+    tempreg |= ( pI2CHandle->I2C_Config.I2C_ACKControl << 10 );
+    pI2CHandle->pI2Cx->CR1 = tempreg;
+
+    // Configure the FREQ field of CR2 (CR2)
+    tempreg = 0;
+    tempreg |= RCC_GetPCLK1Value() / 1000000U;
+    pI2CHandle->pI2Cx->CR2 = ( tempreg & 0x3F ); // We are bit masking the first 6 bits for safety purposes.
+
+    // Program device's own address (OAR1)
+    tempreg |= pI2CHandle->I2C_Config.I2C_DeviceAddress << 1;
+    tempreg |= ( 1 << 14 );
+    pI2CHandle->pI2Cx->OAR1 = tempreg;
 }
 
 /**
