add254890e9fbb7961d235bf0ff03ad802cdaf41
H-Matus <matusevicius.henrikas@gmail.com>
2023-05-24 22:57:35 +0100

    Added ds1307 functions to the source file.

--- target/stm32f4xx_drivers/bsp/ds1307.c
+++ target/stm32f4xx_drivers/bsp/ds1307.c
@@ -5,4 +5,29 @@
  *      Author: Admin
  */
 
+#include "ds1307.h"
 
+uint8_t ds1307_init(void)
+{
+
+}
+
+void ds1307_set_current_time(RTC_time_t *rtc_time)
+{
+
+}
+
+void ds1307_get_current_time(RTC_time_t *rtc_time)
+{
+
+}
+
+void ds1307_set_current_date(RTC_date_t *rtc_date)
+{
+
+}
+
+void ds1307_get_current_date(RTC_date_t *rtc_date)
+{
+
+}
