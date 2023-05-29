de19f54ce3c940bc4232188672e4f4590a8f2209
H-Matus <matusevicius.henrikas@gmail.com>
2023-05-29 14:59:38 +0100

    Adding getter and setter functions.

--- target/stm32f4xx_drivers/bsp/ds1307.c
+++ target/stm32f4xx_drivers/bsp/ds1307.c
@@ -39,19 +39,95 @@ uint8_t ds1307_init(void)
     return ((clock_state >> 7) & 0x1);
 }
 
+uint8_t binary_to_bcd(uint8_t binary)
+{
+    uint8_t bcd;
+
+    return bcd;
+}
+
+uint8_t bcd_to_binary(uint8_t bcd)
+{
+    uint8_t binary;
+
+    return binary;
+}
+
 void ds1307_set_current_time(RTC_time_t *rtc_time)
 {
+    uint8_t seconds, minutes, hours;
 
+    seconds = binary_to_bcd(rtc_time->seconds);
+    minutes = binary_to_bcd(rtc_time->minutes);
+    hours = binary_to_bcd(rtc_time->hours);
+    
+    seconds &= ~(1 << 7);
+
+    ds1307_write(seconds, DS1307_ADDR_SEC);
+    ds1307_write(minutes, DS1307_ADDR_MIN);
+
+    if(rtc_time->time_format == TIME_FORMAT_24HRS)
+    {
+        hours &= ~(1 << 6);
+    }
+    else if(rtc_time->time_format == TIME_FORMAT_12HRS_AM)
+    {
+        hours |= (1 << 6);
+        hours &= ~(1 << 5);
+    }
+    else if(rtc_time->time_format == TIME_FORMAT_12HRS_PM)
+    {
+        hours |= (1 << 6);
+        hours |= (1 << 5);
+    }
+
+    ds1307_write(hours, DS1307_ADDR_HRS);
 }
 
 void ds1307_get_current_time(RTC_time_t *rtc_time)
 {
+    uint8_t seconds, minutes, hours;
 
+    seconds = ds1307_read(DS1307_ADDR_SEC);
+    seconds &= ~(1 << 7);
+
+    minutes = ds1307_read(DS1307_ADDR_MIN);
+
+    rtc_time->seconds = bcd_to_binary(seconds);
+    rtc_time->minutes = bcd_to_binary(minutes);
+
+    hours = ds1307_read(DS1307_ADDR_HRS);
+
+    if(0 == (hours >> 6))
+    {
+        rtc_time->time_format = TIME_FORMAT_24HRS;
+    }
+    else if(2 == (hours >> 5))
+    {
+        rtc_time->time_format = TIME_FORMAT_12HRS_AM;
+    }
+    else if(3 == (hours >> 5))
+    {
+        rtc_time->time_format = TIME_FORMAT_12HRS_PM;
+    }
+
+    hours &= ~(3 << 5);
+    rtc_time->hours = bcd_to_binary(hours);
 }
 
 void ds1307_set_current_date(RTC_date_t *rtc_date)
 {
+    uint8_t day, date, month, year;
 
+    day = binary_to_bcd(rtc_date->day);
+    date = binary_to_bcd(rtc_date->date);
+    month = binary_to_bcd(rtc_date->month);
+    year = binary_to_bcd(rtc_date->year);
+
+    ds1307_write(day, DS1307_ADDR_DAY);
+    ds1307_write(date, DS1307_ADDR_DATE);
+    ds1307_write(month, DS1307_ADDR_MONTH);
+    ds1307_write(year, DS1307_ADDR_YEAR);
 }
 
 void ds1307_get_current_date(RTC_date_t *rtc_date)
